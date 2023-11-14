2b98494f20ec757624ede37b2029f258c711b375
joethi <Jothi.Thondiraj@colorado.edu>
2023-11-14 15:21:11 -0700

    backing up before commiting

--- scripts/GenMod-org-Hmt/genmod/utils.py
+++
@@ -1,6 +0,0 @@
-import pickle
-
-
-def pickle_save(obj, filename, outdir):
-    with open(outdir + '/' + filename + '.pkl', 'wb') as output:
-        pickle.dump(obj, output, pickle.HIGHEST_PROTOCOL)
